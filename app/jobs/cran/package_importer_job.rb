953a9c9181ed5358bc9c83892bae95cdfe850504
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2026-08-23 22:08:06 +0200

    Create Cran::PackageListCheckerJob/

--- app/jobs/cran/package_importer_job.rb
+++ app/jobs/cran/package_importer_job.rb
@@ -9,7 +9,12 @@ class Cran::PackageImporterJob < ApplicationJob
         Zlib::GzipReader.open(URI.open("https://cran.r-project.org/src/contrib/#{name}_#{version}.tar.gz"))
       ).seek("#{name}/DESCRIPTION") { |file| file.read }
 
-    Cran::Package.create!(parse_dcf(description).select { |key, _value| key.in?(Cran::Package.column_names) })
+    return false if description.blank?
+
+    Cran::Package.create!(
+      parse_dcf(description).transform_keys { |key| key.downcase.gsub("/", "_") }.
+      select { |key, _value| key.in?(Cran::Package.column_names) }
+    )
   end
 
   private
@@ -21,7 +26,7 @@ class Cran::PackageImporterJob < ApplicationJob
     text.each_line(chomp: true) do |line|
       if !line.start_with?("\s")
         split_line = line.split(": ", 2)
-        current_key = split_line.first.downcase.gsub("/", "_")
+        current_key = split_line.first
         result[current_key] = split_line.last
       else
         result[current_key] << " " << line.strip
---
+++ app/jobs/cran/package_list_checker_job.rb
@@ -0,0 +1,16 @@
+class Cran::PackageListCheckerJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    URI.open("https://cran.r-project.org/src/contrib/PACKAGES").read.lines(chomp: true).select do |line|
+      line.starts_with?("Package", "Version")
+    end.each_slice(2) do |slice|
+      name = slice.first.split(": ").last
+      version = slice.last.split(": ").last
+
+      unless PackageVersion.exists?(package_id: Package.find_by(name: name)&.id, version: version)
+        Cran::PackageImporterJob.perform_later(name, version)
+      end
+    end
+  end
+end
