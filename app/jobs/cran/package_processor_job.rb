ad15b9e3291a6632d91830464a8769758dd2a938
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2026-08-23 21:14:12 +0200

    Create Cran::PackageProcessorJob.

---
+++ app/jobs/cran/package_processor_job.rb
@@ -0,0 +1,40 @@
+class Cran::PackageProcessorJob < ApplicationJob
+  queue_as :default
+
+  def perform(cran_package_id)
+    return if cran_package_id.blank?
+
+    cran_package = Cran::Package.find_by(id: cran_package_id)
+    return if cran_package.blank?
+
+    package = Package.find_or_initialize_by(name: cran_package.package)
+    package.title = cran_package.title
+    package.description = cran_package.description
+    package.save!
+
+    package_version =
+      PackageVersion.create!(
+        package: package,
+        version: cran_package.version,
+        cran_package: cran_package,
+        published_at: cran_package.date_publication
+      )
+
+    cran_package.author.map { |e| e[0..(e.index("[") - 2)] }.each do |author_name|
+      contributor = Contributor.find_or_create_by!(name: author_name)
+
+      PackageVersionContributor.create!(
+        package_version: package_version,
+        contributor: contributor,
+        is_author: true
+      )
+    end
+
+    email_start = cran_package.maintainer.index("<") + 1
+    email_end = cran_package.maintainer.index(">")
+    maintainer_name = cran_package.maintainer[0..(email_start - 3)]
+    maintainer_email = cran_package.maintainer[email_start...email_end]
+    maintainer = Contributor.find_or_create_by!(name: maintainer_name)
+    maintainer.update!(email: maintainer_email)
+  end
+end
--- app/models/cran/package.rb
+++ app/models/cran/package.rb
@@ -1,3 +1,5 @@
 class Cran::Package < ApplicationRecord
   has_one :package_version
+
+  after_create { Cran::PackageProcessorJob.perform_later(id) }
 end
