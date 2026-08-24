98fb6756d2103ff81509b8e8ef164c0057b48390
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2026-08-24 22:10:07 +0200

    Add sorting before display.

--- app/controllers/package_versions_controller.rb
+++ app/controllers/package_versions_controller.rb
@@ -1,5 +1,5 @@
 class PackageVersionsController < ApplicationController
   def index
-    @package_versions = PackageVersion.includes(:package, :contributors)
+    @package_versions = PackageVersion.includes(:package, :contributors).order(packages: { name: :asc }, version: :desc)
   end
 end
